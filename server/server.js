a38a696d27cc873e3740ff96d623dfaef36ffffa
Robel <rofikre@yahoo.com>
2022-04-23 11:31:09 -0400

    set up Apollo server to use GraphQL queries and mutations

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
+
+const {typeDefs, resolvers} = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,8 +28,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
