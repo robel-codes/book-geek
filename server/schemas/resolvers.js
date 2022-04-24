e4c2a4d00400852d250223f031899069ecaa25af
Robel <rofikre@yahoo.com>
2022-04-24 13:52:51 -0400

    created resolvers and implemented auth middleware

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,24 +1,28 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Book } = require('../models')
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    Users: async () => {
-      return User.find()
-        .select('-__v -password')
-        .populate('savedBooks')
-    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({_id: context.user._id})
+          .select('-__v -password')
+          .populate('saveBooks');
+        
+          return userData;
+      }
 
-    user: async (parent, { username }) => {
-      return User.findOne({ username })
-        .select('-__v -password')
-        .populate('savedBooks')
-    }    
+      throw new AuthenticationError('Not logged in');
+    }
   },
 
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
+      const token = signToken(user)
+
+      return { token, user };
     },
 
     login: async (parent, { email, password }) => {
@@ -28,12 +32,45 @@ const resolvers = {
         throw new AuthenticationError('Incorrect credentials')
       }
 
+      const correctPw = await user.isCorrectPassword(password);
+
       if (!correctPw) {
         throw new AuthenticationError('Incorrect Credentials')
       }
+
+      const token = signToken(user);
+      return { token, user }
+    },
+
+    saveBook: async (parent, { book }, context) => {
+    if (context.user) {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: book } },
+        { new: true }
+      );
+
+      return updatedUser;
     }
+      throw new AuthenticationError('you need to be logged in!');
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId } }},
+          { new: true}
+        )
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    }
+    
   }
 
-}
+};
   
-  module.exports = resolvers;
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const {typeDefs, resolvers} = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: authMiddleware,
   });
   await server.start();
   server.applyMiddleware({ app });
